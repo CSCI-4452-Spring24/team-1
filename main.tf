6bee01d9b3657f67cf95a318e2f78bd0de319f92
treyhollander <70235593+treyhollander@users.noreply.github.com>
2024-04-25 03:09:09 -0500

    3rd rebuild of terraform gave up on K8s finally and we are switching to a Fargate instance instead as PVC with K8s was too big a task

--- main.tf
+++ main.tf
@@ -1,283 +1,116 @@
-// This module creates a single EC2 instance for running a Minecraft server
-
-// Default network
-data "aws_vpc" "default" {
-  default = true
+provider "aws" {
+  region = "us-east-1"
 }
 
-data "aws_subnet_ids" "default" {
-  vpc_id = local.vpc_id
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "minecraft_vpc"
+  cidr = "10.0.0.0/16"
+  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
-data "aws_caller_identity" "aws" {}
+resource "aws_ecs_cluster" "minecraft_server" {
+  name = "minecraft_server"
+}
 
-locals {
-  vpc_id    = length(var.vpc_id) > 0 ? var.vpc_id : data.aws_vpc.default.id
-  subnet_id = length(var.subnet_id) > 0 ? var.subnet_id : sort(data.aws_subnet_ids.default.ids)[0]
-  tf_tags = {
-    Terraform = true,
-    By        = data.aws_caller_identity.aws.arn
+resource "aws_security_group" "minecraft_server" {
+  name        = "minecraft_server"
+  description = "minecraft_server"
+  vpc_id      =  module.vpc.vpc_id
+ 
+  ingress {
+    description = "minecraft_server"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
-// Keep labels, tags consistent
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
-
-  namespace   = var.namespace
-  stage       = var.environment
-  name        = var.name
-  delimiter   = "-"
-  label_order = ["environment", "stage", "name", "attributes"]
-  tags        = merge(var.tags, local.tf_tags)
-}
-
-// Amazon Linux2 AMI - can switch this to default by editing the EC2 resource below
-data "aws_ami" "amazon-linux-2" {
-  most_recent = true
-
-  owners = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
-// Find latest Ubuntu AMI, use as default if no AMI specified
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-// S3 bucket for persisting minecraft
-resource "random_string" "s3" {
-  length  = 12
-  special = false
-  upper   = false
-}
-
-#data "aws_s3_bucket" "selected" {
-#  bucket = local.bucket
-#}
-
-locals {
-  using_existing_bucket = signum(length(var.bucket_name)) == 1
-
-  bucket = length(var.bucket_name) > 0 ? var.bucket_name : "${module.label.id}-${random_string.s3.result}"
-}
-
-module "s3" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  create_bucket = local.using_existing_bucket ? false : true
-
-  bucket = local.bucket
-  acl    = "private"
-
-  force_destroy = var.bucket_force_destroy
-
-  versioning = {
-    enabled = var.bucket_object_versioning
-  }
-
-  # S3 bucket-level Public Access Block configuration
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-
-  tags = module.label.tags
-}
-
-// IAM role for S3 access
-resource "aws_iam_role" "allow_s3" {
-  name   = "${module.label.id}-allow-ec2-to-s3"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
+resource "aws_ecs_task_definition" "minecraft_server" {
+  cpu                      = "4096"
+  memory                   = "8192"
+  family                   = "minecraft-server"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  execution_role_arn = aws_iam_role.ecs_tasks_execution_role.arn
+  container_definitions = jsonencode([
     {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
+      name          = "minecraft-server"
+      image         = "itzg/minecraft-server:java17-alpine"
+      essential     = true
+      tty           = true
+      stdin_open    = true
+      restart       = "unless-stopped"
+      portMappings  = [
+        {
+          containerPort = 25565
+          hostPort      = 25565
+          protocol      = "tcp"
+        }
+      ]
+      environment   = [
+        {
+          name  = "EULA"
+          value = "TRUE"
+        },
+        {
+          "name": "VERSION",
+          "value": "1.19.3"
+        }
+      ]
+      mountPoints   = [
+        {
+          containerPath = "/data"
+          sourceVolume  = "minecraft-data"
+        }
+      ]
     }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_instance_profile" "mc" {
-  name = "${module.label.id}-instance-profile"
-  role = aws_iam_role.allow_s3.name
-}
-
-resource "aws_iam_role_policy" "mc_allow_ec2_to_s3" {
-  name   = "${module.label.id}-allow-ec2-to-s3"
-  role   = aws_iam_role.allow_s3.id
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["arn:aws:s3:::${local.bucket}"]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "s3:PutObject",
-        "s3:GetObject",
-        "s3:DeleteObject"
-      ],
-      "Resource": ["arn:aws:s3:::${local.bucket}/*"]
-    }
-  ]
-}
-EOF
-}
-
-// Script to configure the server - this is where most of the magic occurs!
-data "template_file" "user_data" {
-  template = file("${path.module}/user_data.sh")
-
-  vars = {
-    mc_root        = var.mc_root
-    mc_bucket      = local.bucket
-    mc_backup_freq = var.mc_backup_freq
-    mc_version     = var.mc_version
-    mc_type        = var.mc_type   
-    java_mx_mem    = var.java_mx_mem
-    java_ms_mem    = var.java_ms_mem
+  ])
+  volume {
+    name = "minecraft-data"
   }
 }
 
-// Security group for our instance - allows SSH and minecraft 
-module "ec2_security_group" {
-  source = "git@github.com:terraform-aws-modules/terraform-aws-security-group.git?ref=master"
-
-  name        = "${var.name}-ec2"
-  description = "Allow SSH and TCP ${var.mc_port}"
-  vpc_id      = local.vpc_id
-
-  ingress_cidr_blocks      = [ var.allowed_cidrs ]
-  ingress_rules            = [ "ssh-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = var.mc_port
-      to_port     = var.mc_port
-      protocol    = "tcp"
-      description = "Minecraft server"
-      cidr_blocks = var.allowed_cidrs
-    },
-  ]
-  egress_rules = ["all-all"]
-
-  tags = module.label.tags
+resource "aws_ecs_service" "minecraft_server" {
+  name            = "minecraft_server"
+  cluster         = aws_ecs_cluster.minecraft_server.id
+  task_definition = aws_ecs_task_definition.minecraft_server.arn
+  desired_count   = 1
+  network_configuration {
+    subnets          = module.vpc.public_subnets
+    security_groups  = [aws_security_group.minecraft_server.id]
+    assign_public_ip = true
+  }
+  launch_type = "FARGATE"
 }
 
-// Create EC2 ssh key pair
-resource "tls_private_key" "ec2_ssh" {
-  count = length(var.key_name) > 0 ? 0 : 1
+data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-  algorithm = "RSA"
-  rsa_bits  = 4096
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
 }
 
-resource "aws_key_pair" "ec2_ssh" {
-  count = length(var.key_name) > 0 ? 0 : 1
-
-  key_name   = "${var.name}-ec2-ssh-key"
-  public_key = tls_private_key.ec2_ssh[0].public_key_openssh
+resource "aws_iam_role" "ecs_tasks_execution_role" {
+  name               = "ecs-task-execution-role"
+  assume_role_policy = "${data.aws_iam_policy_document.ecs_tasks_execution_role.json}"
 }
 
-locals {
-  _ssh_key_name = length(var.key_name) > 0 ? var.key_name : aws_key_pair.ec2_ssh[0].key_name
-}
-
-// EC2 instance for the server - tune instance_type to fit your performance and budget requirements
-module "ec2_minecraft" {
-  source = "git::https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git?ref=master"
-  name   = "${var.name}-public"
-
-  # instance
-  key_name             = local._ssh_key_name
-  ami                  = var.ami != "" ? var.ami : data.aws_ami.ubuntu.image_id
-  instance_type        = var.instance_type
-  iam_instance_profile = aws_iam_instance_profile.mc.id
-  user_data            = data.template_file.user_data.rendered
-
-  # network
-  subnet_id                   = local.subnet_id
-  vpc_security_group_ids      = [ module.ec2_security_group.this_security_group_id ]
-  associate_public_ip_address = var.associate_public_ip_address
-
-  tags = module.label.tags
-output "vpc_id" {
-  value = local.vpc_id
-}
-
-output "subnet_id" {
-  value = local.subnet_id
-}
-
-output "public_ip" {
-  value = module.ec2_minecraft.public_ip
-}
-
-output "id" {
-  value = module.ec2_minecraft.id
-}
-
-output "public_key_openssh" {
-  value = tls_private_key.ec2_ssh.*.public_key_openssh
-}
-
-output "public_key" {
-  value = tls_private_key.ec2_ssh.*.public_key_pem
-}
-
-output "private_key" {
-  value = tls_private_key.ec2_ssh.*.private_key_pem
-}
-
-resource "local_file" "private_key" {
-  count = length(var.key_name) > 0 ? 0 : 1
-
-  content              = tls_private_key.ec2_ssh[0].private_key_pem
-  filename             = "${path.module}/ec2-private-key.pem"
-  directory_permission = "0700"
-  file_permission      = "0700"
-}
-
-output "zzz_ec2_ssh" {
-  value = length(var.key_name) > 0 ? "" : <<EOT
-
-Ubuntu: ssh -i ${path.module}/ec2-private-key.pem ubuntu@${module.ec2_minecraft.public_ip[0]}
-Amazon Linux: ssh -i ${path.module}/ec2-private-key.pem ec2-user@${module.ec2_minecraft.public_ip[0]}
-
-EOT
-
-}
-
-output "ec2_instance_profile" {
-  value = "${aws_iam_instance_profile.mc.name}"
-}
-
-output "minecraft_server" {
-  value = "${module.ec2_minecraft[0].public_ip}:${var.mc_port}"
+resource "aws_iam_role_policy_attachment" "ecs_tasks_execution_role" {
+  role       = "${aws_iam_role.ecs_tasks_execution_role.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
